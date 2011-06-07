7e6c0d74f02bdac1e64040bb8bed694345c3703e
GuglielmoS <guglielmos@inventati.org>
2011-06-07 17:58:39 +0200

    added problem 16

---
+++ euler16.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+
+import operator
+from util import chronometer
+
+@chronometer
+def problem_16():
+	return sum(map(int, list(str(2**1000))))
+	
+print problem_16()
