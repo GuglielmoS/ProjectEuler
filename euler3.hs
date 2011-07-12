cb721604b437f71f3cd2b81bc47ca1105aca4a39
GuglielmoS <guglielmos@inventati.org>
2011-07-13 00:35:15 +0200

    added problem 3 in Haskell

---
+++ euler3.hs
@@ -0,0 +1,15 @@
+isPrime :: Int -> Bool
+isPrime n | factorsNumber == 1 = True
+		  | otherwise = False
+		where
+			factorsNumber = length $ factors n
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..limit], n `mod` x == 0]
+	where
+		limit = floor $ sqrt (fromIntegral n) 
+
+primeFactors :: Int -> [Int]
+primeFactors n = filter isPrime (factors n)
+
+problem3 = last (primeFactors 600851475143)
